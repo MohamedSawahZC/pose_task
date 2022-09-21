07f4d7bdf02b0a9b79c1bf782714a6deca64b928
Mohamed Sawah <sawah810@gmail.com>
2022-09-21 05:19:12 +0200

    Handle Use Local database

--- lib/controllers/home_screen/cubit.dart
+++ lib/controllers/home_screen/cubit.dart
@@ -17,80 +17,87 @@ class HomeCubit extends Cubit<HomeStates> {
   List<NoteModel> notes = [];
   List<NoteModel> search = [];
   List<NoteModel> filter = [];
-  List<UserModel> users = [];
+  List<UserModel> users=[];
   late Database database;
 
-  void getAllUsers() {
+  void getAllUsers(){
     emit(GetUsersLoadingState());
-    DioHelper.getData(url: ALLUSERS)
-        .then((value) => {
-              value.data.forEach((e) => {
-                    users.add(UserModel.fromJson(e)),
-                  }),
-              emit(GetUsersSuccessState()),
-            })
-        .catchError((error) {
+    DioHelper.getData(
+        url: ALLUSERS
+    ).then((value) => {
+      value.data.forEach((e)=>{
+        users.add(UserModel.fromJson(e)),
+
+      }),
+      emit(GetUsersSuccessState()),
+    }).catchError((error){
       print(error);
       emit(GetUsersErrorState());
-    });
-  }
 
-  void getAllNotes() async {
-    notes = [];
+    });
+
+  }
+  void getAllNotes() async{
+    notes=[];
     emit(HomeDataLoadingState());
     await DioHelper.getData(
       url: GETNOTES,
     )
         .then((value) => {
-              value.data.forEach((e) => {
-                    notes.add(NoteModel.fromJson(e)),
-                  }),
-              //InsertNotesToLocal(),
-              emit(HomeDataSuccessState()),
-            })
-        .catchError((error) {
-      print(error);
-      bool val = CasheHelper.getData(key: 'switch') ?? false;
-      if (val) {
-        // GetNotes(database);
-      } else {}
-      emit(HomeDataErrorState());
-    });
+         value.data.forEach((e)=>{
+
+                notes.add(NoteModel.fromJson(e)),
+              }),
+
+
+      InsertNotesToLocal(),
+      emit(HomeDataSuccessState()),
+
+    })
+        .catchError((error)  {
+            print(error);
+              bool val = CasheHelper.getData(key: 'switch') ?? false;
+              if(val) {
+                GetNotes(database);
+              }else{
+                emit(HomeDataErrorState());
+              }
+            });
   }
 
   bool DisplaySearch = false;
   bool DisplayFilter = false;
-  void ShowSearch() {
+  void ShowSearch (){
     DisplaySearch = !DisplaySearch;
     emit(DisplaySearchState());
   }
-
-  void ShowFilter() {
+  void ShowFilter (){
     DisplayFilter = !DisplayFilter;
     emit(DisplayFilterState());
   }
 
-  void searchByText(String text) {
-    search = [];
+  void searchByText(String text){
+    search =[];
     notes.forEach((element) {
-      if (element.text.contains(text)) {
+      if(element.text.contains(text)){
         search.add(element);
       }
       emit(SearchByTextState());
     });
+
   }
 
-  void filterByUser(String userId) {
-    filter = [];
+  void filterByUser(String userId){
+    filter =[];
     notes.forEach((element) {
-      if (element.userId == userId) {
+      if(element.userId==userId){
         filter.add(element);
       }
     });
     print(filter.length);
     emit(FilterByUserState());
   }
-/*
+
   void createDatabase() {
     openDatabase(
       'notes.db',
@@ -114,13 +121,21 @@ class HomeCubit extends Cubit<HomeStates> {
     });
   }
   void InsertNotesToLocal() async{
-
+    await database.execute("DROP TABLE IF EXISTS Note").then((value) => print('Droped + ==========='));
+    database
+        .execute(
+        'CREATE TABLE Note(id INTEGER PRIMARY KEY, text TEXT, placeDateTime TEXT, userId TEXT, noteId TEXT)')
+        .then(
+          (value) => print('Table created'+ "======================"),
+    )
+        .catchError((err) => print("err${err.toString()}"));
     notes.forEach((element) async{
       await database.transaction((txn) async {
         txn
             .rawInsert(
             'INSERT INTO Note(text,placeDateTime,userId,noteId) VALUES("${element.text}","${element.placeDateTime}","${element.userId}","${element.id}")')
             .then((value) => {
+              print("Sawahhhhhhhhhhh"),
           emit(HomeDataSuccessState()),
         })
             .catchError((error) {
@@ -149,6 +164,5 @@ class HomeCubit extends Cubit<HomeStates> {
     });
     emit(LocalDataSuccessState());
   }
-  */
 
 }
